47fe8d9773e0c08427f7468893f2ec00a7d663d6
moataz-e <motaz_1993@hotmail.com>
2019-03-02 03:19:37 +0000

    added and configured googletest framework

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <gtest/gtest.h>
+#include "quantset/pricing/options/test_blackscholesmerton.h"
+
+int main(int argc, char *argv[])
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
