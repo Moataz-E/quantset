a1368e6f4d60b8c6b53c171846d0c00a1881ae6f
moataz-e <motaz_1993@hotmail.com>
2019-03-02 00:03:56 +0000

    completed cmake config and added eigen3 linear algebra library

---
+++ quantset/pricing/options/blackscholesmerton.cpp
@@ -0,0 +1,6 @@
+#include "blackscholesmerton.h"
+
+BlackScholesMerton::BlackScholesMerton()
+{
+
+}
---
+++ quantset/pricing/options/blackscholesmerton.h
@@ -0,0 +1,11 @@
+#ifndef BLACKSCHOLESMERTON_H
+#define BLACKSCHOLESMERTON_H
+
+
+class BlackScholesMerton
+{
+public:
+    BlackScholesMerton();
+};
+
+#endif // BLACKSCHOLESMERTON_H
